3333f91aa5c23d47d032cfe8c2305ac337c23f47
M.Aaltokari <marika.aaltokari@gmail.com>
2021-03-30 10:56:11 +0300

    C# ohjelmsitologiikkaa

---
+++ DotNet/Ohjelmistologiikka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ohjelmistologiikka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
