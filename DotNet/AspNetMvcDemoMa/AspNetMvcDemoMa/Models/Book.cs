bd1cea99451af661ce3dbcaf6dd38d0a51ca6411
M.Aaltokari <marika.aaltokari@gmail.com>
2021-04-08 13:33:24 +0300

    end of the day 4/8/2021Inital demo; layaout done but actions are not done

---
+++ DotNet/AspNetMvcDemoMa/AspNetMvcDemoMa/Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace AspNetMvcDemoMa.Models
+{
+    public class Book
+    {
+
+        public int ID { get; set; }
+       // [RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), Required, StringLength(30)]
+        public string AuthorNames { get; set; }
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), Required, StringLength(30)]
+        public string AuthorSurname { get; set; }
+
+       // [StringLength(60, MinimumLength = 3), Required]
+        public string BookTitle { get; set; }
+
+        public string Favorite { get; set; }
+
+
+        //[Display(Name = "Saving Date"), DataType(DataType.Date)]
+        public DateTime SavingDate { get; set; }
+    }
+}
---
+++ DotNet/AspNetMvcDemoMa/AspNetMvcDemoMa/wwwroot/js/Demosovellus.js
@@ -0,0 +1,42 @@
+﻿
+// Avata paikallinen json-tiedosto ja tallentaa uudet inputit paikalliseen json-tiedostoon
+// Marika Aaltokari 8.4.2021 
+<script type="text/javascript" src="~/books.json"></script>
+console.log("Skriptin suoritus alkaa");
+const fs = require('fs');
+
+$("#saving").click(function () {
+    console.log("Nappia painettu");
+    const etunimi = $("#authorNames").val();
+    const sukunimi = $("#authorSurname").val();
+    const nimi = $("#bookTitle").val();
+    const suosikki = new Boolean($("#favorite").val());
+    const päivämäärä = $("#savingDate").val();
+    console.log(etunimi + sukunimi + nimi + suosikki + päivämäärä);
+
+    //konstruktori
+    Book kirja = new Book
+
+    const kirja = {
+        "id": "",
+        "authorNames": etunimi,
+        "authorSurname": sukunimi,
+        "booktitle": nimi,
+        "favorite": suosikki,
+        "savingsDate": päivämäärä,
+    }
+
+    // convert JSON object to string
+    const data = JSON.stringify(kirja);
+    console.log("data:" + data);
+
+    // write JSON string to a file
+    fs.writeFile('books.json', data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("JSON data is saved.");
+        alert("The read book is saved.");
+
+    });
+}
