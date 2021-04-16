a261f4fed90c64615c653adc9d12fa6c25bb62e8
M.Aaltokari <marika.aaltokari@gmail.com>
2021-04-16 11:01:25 +0300

    lisätty BookshelfController

---
+++ DotNet/ApsNetWebApiDemoMa/ApsNetWebApiDemoMa/Controllers/BookApiController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApsNetWebApiDemoMa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookApiController : ControllerBase
+    {
+
+        
+        [HttpGet]
+        [Route("kaikki")]
+        public List<Book> PalautaKaikki()
+        {
+            BookshelfContext konteksti = new();
+            List<Book> kirjat = konteksti.Books.ToList();
+            return kirjat;
+        }
+
+
+        [HttpGet]
+        [Route("search/{krjanNimi}")]
+        public List<Book> PalautaNimenMukaan(string kirjanNimi)
+        {
+            BookshelfContext konteksti = new();
+            List<Book> haettu1 = konteksti.Books.
+                Where(t => t.BookTitle == kirjanNimi).ToList();
+            return haettu1;
+        }
+
+        [HttpGet]
+        [Route("search/{kirjailijanNimi}")]
+        public List<Book> PalautaTekijänMukaan(string )
+        {
+            BookshelfContext konteksti = new();
+            List<Book> haettu2 = konteksti.Books.
+                Where(a => a. Author == ¨kirjailijanNimi).ToList();
+            return haettu2;
+        }
+
+
+        [HttpPost]
+        [Route("")]
+        public void LisääUusi([FromBody] Book uusiKirja)
+        {
+            BookshelfContext konteksti = new();
+            konteksti.Books.Add(uusiKirja);
+            konteksti.SaveChanges();
+        }
+    }
+}
---
+++ DotNet/ApsNetWebApiDemoMa/ApsNetWebApiDemoMa/Mallit/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApsNetWebApiDemoMa.Mallit
+{
+    public class Book
+    {
+
+        //// public int ID { get; set; }
+        // [RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), Required, StringLength(30)]
+        public string AuthorNames { get; set; }
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z]*$"), Required, StringLength(30)]
+        public string AuthorSurname { get; set; }
+
+        public string Author => (string)(AuthorNames+","+ AuthorSurname);
+
+        // [StringLength(60, MinimumLength = 3), Required]
+        public string BookTitle { get; set; }
+
+        public string Favorite { get; set; }
+
+
+        //[Display(Name = "Saving Date"), DataType(DataType.Date)]
+        public DateTime SavingDate { get; set; }
+        
+       
+    }
+}
